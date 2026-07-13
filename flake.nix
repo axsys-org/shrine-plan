c426b77b96df488298f35ffda3ec53479bec4081
Liam <liam@axiomatic.systems>
2026-07-13 12:57:16 -0500

    http driver, compile speedup

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake = false;
   };
   inputs.enki = {
-    url = "github:axsys-org/enki/main";
+    url = "github:axsys-org/enki/lf/http-drivers";
   };
 
   outputs = { self, nixpkgs, rex, enki }:
